ac435fd56b805f97c10d9ce6623b3dbae29968cf
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2026-01-25 17:07:30 +0100

    Updates

---
+++ _app/immutable/chunks/paths.BNfeyX0h.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_192skc8?.base??"",a=globalThis.__sveltekit_192skc8?.assets??s;export{a,s as b};
--- _app/immutable/entry/start.CTnGpFsW.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BU3Zth2L.js";export{t as start};
